c4fe1029f73c6e4c0330748d2a7419c9cbb4217f
G Rohit <g.rohit.eee+github@gmail.com>
2019-06-24 12:54:43 +0530

    fixed projects nav link

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ function scrollFunction() {
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
+  window.scroll({ top: 0, left: 0, behavior: 'smooth' });
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
